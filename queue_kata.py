2c59b7d4e8e67f47eb70e1182a41de9548ae0866
Kharis Simon <8944421+ienchia@users.noreply.github.com>
2019-08-29 13:47:29 +0700

    clean code first iteration

--- queue_kata.py
+++ queue_kata.py
@@ -1,40 +1,48 @@
+from typing import List
+
 class Customer:
-    def __init__(self, id, age, items):
-        self.id = id
-        self.age = age
-        self.items = items # list of string
+    def __init__(self, id: str, age: int, items: List[str]):
+        self.id: str = id
+        self.age: int = age
+        self.items: List[str] = items
+
+    def is_senior(self) -> bool:
+        return self.age >= 62
 
 class Queue:
     def __init__(self):
-        self.items = []
+        self.customers: List[str] = []
+        self.prohibited_items: List[str] = ["cigarette", "alcohol"]
 
-    # Add new item
-    def add_item(self, x):
-        if x.age < 62:
-            if x.age < 18:
-                for item in x.items:
-                    if item in ["cigarette", "alcohol"]:
-                        return
-            self.items.append(x)
-        if x.age >=62:
-            for item in x.items:
-                if item in ["cigarette", "alcohol"]: # prohibited items
+    def add_customer(self, new_customer: Customer) -> None:
+        if not new_customer.is_senior():
+            if new_customer.age < 18:
+                if self.has_non_prohibited_items(new_customer):
                     return
-            pos = 0
-            i = 0
-            while i < len(self.items) and self.items[i].age >= 62:
-                i += 1
-            # print(i)
-            self.items.insert(i, x)
+            self.customers.append(new_customer)
+        if new_customer.is_senior():
+            if self.has_non_prohibited_items(new_customer):
+                return
+            customer_order_based_on_age = 0
+            while customer_order_based_on_age < len(self.customers) and self.customers[customer_order_based_on_age].is_senior():
+                customer_order_based_on_age += 1
+
+            self.customers.insert(customer_order_based_on_age, new_customer)
 
     # Return first item
-    def first(self):
-        return self.items[0]
+    def first(self) -> Customer:
+        return self.customers[0]
+
+    def has_non_prohibited_items(self, customer) -> bool:
+        for item in customer.items:
+            if item in self.prohibited_items:
+                return True
+        return False
 
     # Return last item
-    def last(self):
-        return self.items[-1]
+    def last(self) -> Customer:
+        return self.customers[-1]
 
     # Return queue length
-    def length(self):
-        return len(self.items)
+    def length(self) -> int:
+        return len(self.customers)
