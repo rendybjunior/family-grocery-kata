3eab8da5cd22d47b59b4b98800d27eae3bde0444
khusnanadiatvlk <khusna.nadia@traveloka.com>
2019-08-30 11:29:44 +0700

    add global variable for AGE LIMIT

--- queue_kata.py
+++ queue_kata.py
@@ -7,22 +7,24 @@ class Customer:
 class Queue:
     def __init__(self):
         self.queue = []
+        self.MINOR_AGE_LIMIT = 18
+        self.SENIOR_AGE_LIMIT = 62
 
     # Add new customer queue
     def add_queue(self, customer):
         is_banned_customer = self.is_banned(customer)
 
-        if customer.age < 62:
-            if customer.age < 18 and is_banned_customer:
+        if customer.age < self.SENIOR_AGE_LIMIT:
+            if customer.age < self.MINOR_AGE_LIMIT and is_banned_customer:
                 return
             self.queue.append(customer)
 
-        if customer.age >= 62:
+        if customer.age >= self.SENIOR_AGE_LIMIT:
             if is_banned_customer:
                 return
 
             sequence = 0
-            while sequence < len(self.queue) and self.queue[sequence].age >= 62:
+            while sequence < len(self.queue) and self.queue[sequence].age >= self.SENIOR_AGE_LIMIT:
                 sequence += 1
             # print(i)
             self.queue.insert(sequence, customer)
@@ -33,7 +35,7 @@ class Queue:
         customer_age = customer.age
 
         for item in customer.groceries:
-            if item in banned_items and (customer_age < 18 or customer_age >= 62):
+            if item in banned_items and (customer_age < self.MINOR_AGE_LIMIT or customer_age >= self.SENIOR_AGE_LIMIT):
                 return True
         return False
 
