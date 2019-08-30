2597a482c0b5f42bc20fba0b36d5ff731aa45760
khusnanadiatvlk <khusna.nadia@traveloka.com>
2019-08-30 11:21:13 +0700

    refactor queue_kata

--- queue_kata.py
+++ queue_kata.py
@@ -1,40 +1,50 @@
 class Customer:
-    def __init__(self, id, age, items):
+    def __init__(self, id, age, groceries):
         self.id = id
         self.age = age
-        self.items = items # list of string
+        self.groceries = groceries # list of string
 
 class Queue:
     def __init__(self):
-        self.items = []
+        self.queue = []
 
-    # Add new item
-    def add_item(self, x):
-        if x.age < 62:
-            if x.age < 18:
-                for item in x.items:
-                    if item in ["cigarette", "alcohol"]:
-                        return
-            self.items.append(x)
-        if x.age >=62:
-            for item in x.items:
-                if item in ["cigarette", "alcohol"]: # prohibited items
-                    return
-            pos = 0
-            i = 0
-            while i < len(self.items) and self.items[i].age >= 62:
-                i += 1
+    # Add new customer queue
+    def add_queue(self, customer):
+        is_banned_customer = self.is_banned(customer)
+
+        if customer.age < 62:
+            if customer.age < 18 and is_banned_customer:
+                return
+            self.queue.append(customer)
+
+        if customer.age >= 62:
+            if is_banned_customer:
+                return
+
+            sequence = 0
+            while sequence < len(self.queue) and self.queue[sequence].age >= 62:
+                sequence += 1
             # print(i)
-            self.items.insert(i, x)
+            self.queue.insert(sequence, customer)
+
+    #return true if groceries contains banned items of customer
+    def is_banned(self, customer):
+        banned_items = ["cigarette", "alcohol"]
+        customer_age = customer.age
+
+        for item in customer.groceries:
+            if item in banned_items and (customer_age < 18 or customer_age >= 62):
+                return True
+        return False
 
     # Return first item
     def first(self):
-        return self.items[0]
+        return self.queue[0]
 
     # Return last item
     def last(self):
-        return self.items[-1]
+        return self.queue[-1]
 
     # Return queue length
     def length(self):
-        return len(self.items)
+        return len(self.queue)
