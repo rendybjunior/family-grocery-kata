8d35f8f5a418874f5dbf9145f75373bb7cc47a33
Misbach Imaduddin Zuhdi <misbach.zuhdi@traveloka.com>
2019-09-09 14:34:04 +0700

    implement logic check on IS THRESHOLD

--- queue_kata.py
+++ queue_kata.py
@@ -4,26 +4,27 @@ class Customer:
         self.age = age
         self.groceries = groceries
 
-class Queue:
+class QueueSeniorPrioritization:
+    
+    PROHIBITED_GROCERIES = ["cigarette", "alcohol"]
+    SENIOR_AGE_THRESHOLD = 62
+    ADULT_AGE_THRESHOLD = 18
+    LAST_SENIOR_QUEUE_INDEX = 0
+
     def __init__(self):
         self.queueLine = []
-        self.prohibitedGroceries = ["cigarette", "alcohol"]
-        self.seniorAge = 62
-        self.adultAge = 18
-        self.lastSeniorQueueIndex = 0
 
-    # Add new customer to queue line
     def add_to_queue(self, customer):
 
         def _is_senior(age):
-            return True if age >= self.seniorAge else False
+            return age >= QueueSeniorPrioritization.SENIOR_AGE_THRESHOLD
         
         def _is_adult(age):
-            return True if age >= self.adultAge else False
+            return age >= QueueSeniorPrioritization.ADULT_AGE_THRESHOLD
 
         def _remove_prohobited_grocery(groceries):
             for grocery in groceries:
-                if grocery in self.prohibitedGroceries:
+                if grocery in QueueSeniorPrioritization.PROHIBITED_GROCERIES:
                     groceries.remove(grocery)
 
         if not _is_senior(customer.age):
@@ -34,18 +35,15 @@ class Queue:
         if _is_senior(customer.age):
             _remove_prohobited_grocery(customer.groceries)
             if len(customer.groceries) is not 0:
-                while self.lastSeniorQueueIndex < len(self.queueLine) and _is_senior(self.queueLine[self.lastSeniorQueueIndex].age):
-                    self.lastSeniorQueueIndex += 1
-                self.queueLine.insert(self.lastSeniorQueueIndex, customer)
+                while QueueSeniorPrioritization.LAST_SENIOR_QUEUE_INDEX < len(self.queueLine) and _is_senior(self.queueLine[QueueSeniorPrioritization.LAST_SENIOR_QUEUE_INDEX].age):
+                    QueueSeniorPrioritization.LAST_SENIOR_QUEUE_INDEX += 1
+                self.queueLine.insert(QueueSeniorPrioritization.LAST_SENIOR_QUEUE_INDEX, customer)
 
-    # Return first queue
     def get_first_queue(self):
         return self.queueLine[0]
 
-    # Return last queue
     def get_last_queue(self):
         return self.queueLine[-1]
 
-    # Return queue length
     def get_queue_length(self):
         return len(self.queueLine)
--- test_queue_kata.py
+++ test_queue_kata.py
@@ -1,70 +1,70 @@
-from queue_kata import Customer, Queue
+from QueueSeniorPrioritization_kata import Customer, QueueSeniorPrioritization
 
 def test_firstlast_happypath():
-    q = Queue()
+    q = QueueSeniorPrioritization()
 
-    q.add_to_queue(Customer(1, 6, ["biscuit", "chips"]))
-    q.add_to_queue(Customer(2, 23, ["milk"]))
-    q.add_to_queue(Customer(3, 55, ["soap"]))
+    q.add_to_QueueSeniorPrioritization(Customer(1, 6, ["biscuit", "chips"]))
+    q.add_to_QueueSeniorPrioritization(Customer(2, 23, ["milk"]))
+    q.add_to_QueueSeniorPrioritization(Customer(3, 55, ["soap"]))
 
-    assert q.get_first_queue().id == 1
-    assert q.get_last_queue().id == 3
+    assert q.get_first_QueueSeniorPrioritization().id == 1
+    assert q.get_last_QueueSeniorPrioritization().id == 3
 
 def test_len():
-    q = Queue()
+    q = QueueSeniorPrioritization()
 
-    assert q.get_queue_length() == 0
+    assert q.get_QueueSeniorPrioritization_length() == 0
 
-    q.add_to_queue(Customer(1, 6, ["biscuit", "chips"]))
+    q.add_to_QueueSeniorPrioritization(Customer(1, 6, ["biscuit", "chips"]))
 
-    assert q.get_queue_length() == 1
+    assert q.get_QueueSeniorPrioritization_length() == 1
 
     for i in range(10):
-        q.add_to_queue(Customer(1, 6, ["biscuit", "chips"]))
+        q.add_to_QueueSeniorPrioritization(Customer(1, 6, ["biscuit", "chips"]))
 
-    assert q.get_queue_length() == 11
+    assert q.get_QueueSeniorPrioritization_length() == 11
 
 def test_senior_citizens():
-    q = Queue()
+    q = QueueSeniorPrioritization()
 
-    q.add_to_queue(Customer(1, 6, ["biscuit", "chips"]))
-    q.add_to_queue(Customer(2, 43, ["milk"]))
-    q.add_to_queue(Customer(3, 55, ["soap"]))
-    q.add_to_queue(Customer(4, 70, ["bread"]))
-    q.add_to_queue(Customer(5, 34, ["milk"]))
-    q.add_to_queue(Customer(6, 62, ["soap"]))
+    q.add_to_QueueSeniorPrioritization(Customer(1, 6, ["biscuit", "chips"]))
+    q.add_to_QueueSeniorPrioritization(Customer(2, 43, ["milk"]))
+    q.add_to_QueueSeniorPrioritization(Customer(3, 55, ["soap"]))
+    q.add_to_QueueSeniorPrioritization(Customer(4, 70, ["bread"]))
+    q.add_to_QueueSeniorPrioritization(Customer(5, 34, ["milk"]))
+    q.add_to_QueueSeniorPrioritization(Customer(6, 62, ["soap"]))
 
-    assert [cust.id for cust in q.queueLine] == [4, 6, 1, 2, 3, 5]
+    assert [cust.id for cust in q.QueueSeniorPrioritizationLine] == [4, 6, 1, 2, 3, 5]
 
 def test_all_seniors():
-    q = Queue()
+    q = QueueSeniorPrioritization()
 
-    q.add_to_queue(Customer(1, 62, ["biscuit", "chips"]))
-    q.add_to_queue(Customer(2, 63, ["milk"]))
-    q.add_to_queue(Customer(3, 77, ["soap"]))
-    q.add_to_queue(Customer(4, 70, ["bread"]))
-    q.add_to_queue(Customer(5, 78, ["milk"]))
-    q.add_to_queue(Customer(6, 63, ["soap"]))
+    q.add_to_QueueSeniorPrioritization(Customer(1, 62, ["biscuit", "chips"]))
+    q.add_to_QueueSeniorPrioritization(Customer(2, 63, ["milk"]))
+    q.add_to_QueueSeniorPrioritization(Customer(3, 77, ["soap"]))
+    q.add_to_QueueSeniorPrioritization(Customer(4, 70, ["bread"]))
+    q.add_to_QueueSeniorPrioritization(Customer(5, 78, ["milk"]))
+    q.add_to_QueueSeniorPrioritization(Customer(6, 63, ["soap"]))
 
-    assert [cust.id for cust in q.queueLine] == [1,2,3,4,5,6]
+    assert [cust.id for cust in q.QueueSeniorPrioritizationLine] == [1,2,3,4,5,6]
 
 def test_prohibited_items():
-    q = Queue()
+    q = QueueSeniorPrioritization()
 
-    q.add_to_queue(Customer(1, 16, ["biscuit", "chips", "alcohol"]))
-    q.add_to_queue(Customer(2, 43, ["milk"]))
-    q.add_to_queue(Customer(3, 76, ["alcohol"]))
-    q.add_to_queue(Customer(4, 12, ["pencil"]))
-    q.add_to_queue(Customer(5, 34, ["milk"]))
-    q.add_to_queue(Customer(6, 88, ["soap"]))
+    q.add_to_QueueSeniorPrioritization(Customer(1, 16, ["biscuit", "chips", "alcohol"]))
+    q.add_to_QueueSeniorPrioritization(Customer(2, 43, ["milk"]))
+    q.add_to_QueueSeniorPrioritization(Customer(3, 76, ["alcohol"]))
+    q.add_to_QueueSeniorPrioritization(Customer(4, 12, ["pencil"]))
+    q.add_to_QueueSeniorPrioritization(Customer(5, 34, ["milk"]))
+    q.add_to_QueueSeniorPrioritization(Customer(6, 88, ["soap"]))
 
-    assert [cust.id for cust in q.queueLine] == [6, 1, 2, 4, 5]
+    assert [cust.id for cust in q.QueueSeniorPrioritizationLine] == [6, 1, 2, 4, 5]
 
 
 def test_all_prohibited():
-    q = Queue()
+    q = QueueSeniorPrioritization()
 
-    q.add_to_queue(Customer(1, 16, ["biscuit", "chips", "alcohol"]))
-    q.add_to_queue(Customer(3, 76, ["alcohol"]))
+    q.add_to_QueueSeniorPrioritization(Customer(1, 16, ["biscuit", "chips", "alcohol"]))
+    q.add_to_QueueSeniorPrioritization(Customer(3, 76, ["alcohol"]))
 
-    assert [cust.id for cust in q.queueLine] == [1]
+    assert [cust.id for cust in q.QueueSeniorPrioritizationLine] == [1]
